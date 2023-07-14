47793b1ec3d528192887b27bd63277e6c4d86336
vinipta-s <viniptasabbithi@gmail.com>
2023-07-14 12:13:10 -0400

    test commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sprint_4_project
+This project is a study of car ads data
+
+
+###### hot to lauch on your own 
---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+
+car_ad_data = pd.read_csv('vehicles_us.csv')
+
+st.header('created a table grouping model and year and geting the average price')
+a = car_ad_data.groupby(['model', 'model_year'])['price'].mean().reset_index()
+fig = px.scatter(a, x="model_year", y="price", color="model")
+
+st.write(a)
+st.plotly_chart(fig, use_container_width=True)
